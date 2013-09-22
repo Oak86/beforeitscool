ad3c164668f59a853411082fd3fb3f232736b3bd
oak86 <email@omarkhafagy.com>
2013-09-22 15:05:29 -0400

    add attr_accessible for tag_list to the restaurant model and add option to create new restaurant with tags

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -2,7 +2,8 @@ class Restaurant < ActiveRecord::Base
 	# this is a rails thing - mass assignment
 	attr_accessible :name, :phone, :address, :street, :city, :postal 
 	attr_accessible :opentime, :closetime
-
+	attr_accessible :tag_list
+	acts_as_taggable
 	# this is a ruby thing - this makes getters and setters
 	# attr_accessor
 
